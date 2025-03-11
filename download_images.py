10c4611ed8f8b81ee04028f629b054be3e777e54
Jc-Arroyo <jcarroyos@gmail.com>
2025-03-11 15:38:56 +0000

    actualización de múltiples imágenes en la carpeta de herramientas

--- download_images.py
+++ download_images.py
@@ -3,14 +3,17 @@ import requests
 import os
 
 # Define the CSV file path and the directory to save images
-csv_file_path = '/Users/imac/Desktop/scrap_design_methods/scraped_data.csv'
-images_dir = '/Users/imac/Desktop/scrap_design_methods/images'
+csv_file_path = './scraped_data.csv'
+images_dir = './images'
 
 # Create the images directory if it doesn't exist
 os.makedirs(images_dir, exist_ok=True)
 
 # Function to download an image from a URL
 def download_image(url, save_path):
+    if not url.startswith(('http://', 'https://')):
+        print(f"Invalid URL: {url}")
+        return
     response = requests.get(url)
     if response.status_code == 200:
         with open(save_path, 'wb') as file:
@@ -24,6 +27,6 @@ with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=';')
     for row in csv_reader:
         image_url = row['Image Path']
-        image_name = os.path.basename(image_url)
+        image_name = os.path.basename(image_url).split('?')[0]  # Remove query parameters
         save_path = os.path.join(images_dir, image_name)
         download_image(image_url, save_path)
